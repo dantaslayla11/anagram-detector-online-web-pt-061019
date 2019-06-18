222519aa4550d863b6e730eeeb4a5c0a3fad56f5
layla dantas <laylatdantas@gmail.com>
2019-06-18 03:50:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,13 +7,11 @@ class Anagram
  end 
  
  def match(array)
-   puts array
-   match_word = @word.split("").sort
+  match_word = @word.split("").sort
    result = []
    array.each do |item|
      match_item = item.split("").sort
      if match_word==match_item
-      puts item
       result.push(item)
     end
    end
